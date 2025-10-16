dd5247a85d66b823c6df82ac011b15c3bcbf3baa
Pajul7 <cyberp.lejoueur@gmail.com>
2025-10-16 15:56:10 +0200

    documenting functions in header.

--- bmp_utils.h
+++ bmp_utils.h
@@ -33,16 +33,66 @@ static const Color MAGENTA =   {255,0  ,255,255};
 static const Color YELLOW =    {255,255,0  ,255};
 static const Color CYAN =      {0  ,255,255,255};
 
-
+/**
+ * Reads data from the given file, 
+ * and creates a new BMP_IMG structure with it.
+ *  
+ * filepath : must be a valid file path. Will exit if the file is not a bitmap file. 
+ */
 BMP_IMG importBMP(const char * filepath);
+
+/**
+ * Take an image pointer, valid pixel coordinates x y,
+ * and returns the selected pixel's color of type Color.
+ */
 Color getPixel(BMP_IMG * img, int x, int y);
+
+/**
+ * Take an image pointer, valid pixel coordinates x y and a Color,
+ * and set the selected pixel to the given Color.
+ */
 void putPixel(BMP_IMG * img, int x, int y, Color c);
+
+/**
+ * Take an image pointer,
+ * and the image will be printed in the standard output (i.e. the current terminal emulator)
+ * since the image is a binary image composed of only black and white,
+ * the function will consider a pixel black if the blue value is at 255.
+ */
 void printBMP(BMP_IMG * img);
+
+/**
+ * Creates a new BMP_IMG struct of given height, width and color depth.
+ * all pixels are set to white.
+ * Returns a pointer to the new image.
+ */
 BMP_IMG * newBMP( int height, int width, int bpp );
+
+/**
+ * Creates an exact independant copy of a BMP_IMG struct, 
+ * and returns a pointer to it.
+ */
 BMP_IMG * duplicateBMP(BMP_IMG * src);
+
+/**
+ * Frees all memory used by a BMP_IMG.
+ */
 void destroyBMP(BMP_IMG * img);
+
+/**
+ * Takes a BMP_IMG struct and a path and creates a .bmp file 
+ * at the filename's location from it.
+ */
 void exportBMP(BMP_IMG img, const char * filename);
+
+/**
+ * Set all pixels from the referenced image pointer to the given color. 
+ */
 void clearColor(BMP_IMG * img, Color c);
+
+/**
+ * Checks wether color a and color b are the same.
+ */
 bool isSameColor(Color a , Color b);
 
 
